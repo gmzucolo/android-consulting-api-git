fd67fa8947092eb0451e048316bbdee12190ec0c
gmzucolo <gustavo.zucolo@hotmail.com>
2022-03-19 19:07:53 -0300

    Recyclerview of RepositoryFragment.kt implemented

--- app/src/main/java/com/example/gmzucolo/appconsultingapigit/RepositoryFragment.kt
+++ app/src/main/java/com/example/gmzucolo/appconsultingapigit/RepositoryFragment.kt
@@ -7,6 +7,10 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.gmzucolo.appconsultingapigit.model.PullRequest
+import layout.RepositoryAdapter
 
 class RepositoryFragment : Fragment() {
 
@@ -18,11 +22,35 @@ class RepositoryFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_repository, container, false)
         val txtView2 = view.findViewById<TextView>(R.id.txtView2)
 
+        val recyclerView: RecyclerView = view.findViewById(R.id.recyclerViewListPullRequest)
+        recyclerView.adapter = RepositoryAdapter(pullrequests(), this.requireContext())
+        val layoutManager = StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL)
+        recyclerView.layoutManager = layoutManager
+
         txtView2.setOnClickListener {
             Navigation.findNavController(view)
                 .navigate(R.id.action_repositoryFragment_to_listRepositoryFragment)
         }
 
         return view
+    }
+
+    private fun pullrequests(): List<PullRequest> {
+        return listOf(
+            PullRequest("Gustavo Repo", "PullReques 01", "lalala"),
+            PullRequest("Gustavo Repo", "PullReques 01", "lalala"),
+            PullRequest("Gustavo Repo", "PullReques 01", "lalala"),
+            PullRequest("Gustavo Repo", "PullReques 01", "lalala"),
+            PullRequest("Gustavo Repo", "PullReques 01", "lalala"),
+            PullRequest("Gustavo Repo", "PullReques 01", "lalala"),
+            PullRequest("Gustavo Repo", "PullReques 01", "lalala"),
+            PullRequest("Gustavo Repo", "PullReques 01", "lalala"),
+            PullRequest("Gustavo Repo", "PullReques 01", "lalala"),
+            PullRequest("Gustavo Repo", "PullReques 01", "lalala"),
+            PullRequest("Gustavo Repo", "PullReques 01", "lalala"),
+            PullRequest("Gustavo Repo", "PullReques 01", "lalala"),
+            PullRequest("Gustavo Repo", "PullReques 01", "lalala"),
+        )
+
     }
 }
---
+++ app/src/main/java/com/example/gmzucolo/appconsultingapigit/model/PullRequest.kt
@@ -0,0 +1,9 @@
+package com.example.gmzucolo.appconsultingapigit.model
+
+data class PullRequest(
+//    val id: Long = 0,
+    val repoName: String = "",
+    val title: String = "",
+    val description: String = "",
+//    val state: String = ""
+)
